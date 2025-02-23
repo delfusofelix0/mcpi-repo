bbcfab6aea7493bb329f597a7cb06d4445105d3e
Raven Ortiz <atchupbulad15@gmail.com>
2025-02-23 20:29:39 +0800

    style: add components

---
+++ applicant-app/resources/js/Pages/PositionTable.vue
@@ -0,0 +1,15 @@
+<script setup>
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';   // optional
+import Row from 'primevue/row';                   // optional
+
+</script>
+
+<template>p
+
+</template>
+
+<style scoped>
+
+</style>
