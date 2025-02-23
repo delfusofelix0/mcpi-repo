06d256ae6b32652ee0ceda8fe09127615a7e1b79
PerFect <atchupbulad15@gmail.com>
2025-02-24 01:02:32 +0800

    feat: add crud to the applicant table😜

--- applicant-app/app/Http/Controllers/ApplicantController.php
+++ applicant-app/app/Http/Controllers/ApplicantController.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Position;
-use App\Models\Applicant;
+use App\Models\WorkPosition;
 use App\Models\Registration;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -12,7 +11,16 @@ class ApplicantController extends Controller
 {
     public function index()
     {
-        $query = Registration::latest();
+        $query = Registration::select([
+            'id',
+            'first_name',
+            'last_name',
+            'email',
+            'highest_education',
+            'work_position_id',
+            'created_at',
+            'status'
+        ])->with('position:id,name')->latest();
 
         $registrations = $query->get();
 
@@ -27,5 +35,41 @@ class ApplicantController extends Controller
         return Inertia::render('Dashboard', [
             'applicant' => $applicant
         ]);
+    }
+
+    public function statusStore(Request $request, $id)
+    {
+        $request->validate([
+            'status' => 'required|in:Pending,Hired,Option,Viewed,Rejected',
+        ]);
+
+        $registration = Registration::findOrFail($id);
+        $registration->status = $request->status;
+        $registration->save();
+
+        return redirect()->back()->with('success', 'Applicant status updated successfully.');
+    }
+
+    public function show($id)
+    {
+        $applicant = Registration::with(['documents' => function ($query) {
+            $query->select('id', 'registration_id', 'document_type', 'file_path');
+        }])->findOrFail($id);
+
+        return Inertia::render('Applicant/ApplicantInformation', [
+            'applicant' => $applicant
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        try {
+            $registration = Registration::findOrFail($id);
+            $registration->delete();
+
+            return redirect()->route('dashboard')->with('success', 'Applicant deleted successfully');
+        } catch (\Exception $e) {
+            return redirect()->route('dashboard')->with('error', 'Failed to delete applicant');
+        }
     }
 }
