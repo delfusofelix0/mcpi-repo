84a8a4bd4fc47e762f28f3c6c728df8aa1e9423c
PerFect <atchupbulad15@gmail.com>
2025-04-13 22:43:23 +0800

    feat(appointment): add StoreRequest for the appointment

--- applicant-app/app/Http/Requests/StoreAppointmentRequest.php
+++ applicant-app/app/Http/Requests/StoreAppointmentRequest.php
@@ -23,9 +23,10 @@ class StoreAppointmentRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
-            'contact' => ['required', 'string', 'max:255'],
+            'contact' => ['required', 'string', 'regex:/^(09|\+639|639)\d{9}$/', 'max:13'],
             'date' => ['required', 'date', 'after_or_equal:today'],
-            'time' => ['required', 'string', 'regex:/^([0-1]?[0-9]|2[0-3]):[0-5][0-9] - ([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/'],
+            'time' => ['required', 'string', 'regex:/^([1-9]|1[0-2]):00 (AM|PM) - ([1-9]|1[0-2]):00 (AM|PM)$/'],
+            'office_id' => ['required', 'exists:offices,id'],
         ];
     }
 
@@ -45,7 +46,9 @@ class StoreAppointmentRequest extends FormRequest
             'date.date' => 'The appointment date must be a valid date.',
             'date.after_or_equal' => 'The appointment date must be today or a future date.',
             'time.required' => 'Please select an appointment time.',
-            'time.regex' => 'The appointment time must be in the format HH:MM - HH:MM.',
+            'time.regex' => 'The appointment time must be in the format "HH:00 AM/PM - HH:00 AM/PM".',
+            'office_id.required' => 'Please select an office.',
+            'office_id.exists' => 'The selected office is invalid.',
         ];
     }
 }
