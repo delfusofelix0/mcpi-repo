63fa652f7f77f0fe9662d664a6a7abf61aaf7b41
PerFect <atchupbulad15@gmail.com>
2025-04-13 22:41:59 +0800

    deps: change from TW-based primevue to primevue styled mode

--- applicant-app/resources/js/app.js
+++ applicant-app/resources/js/app.js
@@ -8,9 +8,29 @@ import {createApp, h} from 'vue';
 import {ZiggyVue} from '../../vendor/tightenco/ziggy';
 
 import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import {definePreset, palette} from '@primeuix/themes';
 import ToastService from 'primevue/toastservice';
 
 const appName = import.meta.env.VITE_APP_NAME || 'MCPI';
+
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{sky.50}',
+            100: '{sky.100}',
+            200: '{sky.200}',
+            300: '{sky.300}',
+            400: '{sky.400}',
+            500: '{sky.500}',
+            600: '{sky.600}',
+            700: '{sky.700}',
+            800: '{sky.800}',
+            900: '{sky.900}',
+            950: '{sky.950}'
+        }
+    }
+});
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -24,8 +44,14 @@ createInertiaApp({
             .use(plugin)
             .use(ZiggyVue)
             .use(PrimeVue, {
-                theme: 'none',
-                ripple: true,
+                theme: {
+                    preset: MyPreset,
+                    options: {
+                        prefix: 'p',
+                        darkModeSelector: '.my-app-dark',
+                        cssLayer: false
+                    }
+                }
             })
             .use(ToastService)
             .mount(el);
