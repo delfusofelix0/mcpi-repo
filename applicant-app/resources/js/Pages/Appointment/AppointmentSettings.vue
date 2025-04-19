51a5f948c0e27df59d2a1d4f84f85c3c30fc0f6c
PerFect <atchupbulad15@gmail.com>
2025-04-20 05:12:43 +0800

    add appointment list props🤷‍♀️

--- applicant-app/resources/js/Pages/Appointment/AppointmentSettings.vue
+++ applicant-app/resources/js/Pages/Appointment/AppointmentSettings.vue
@@ -2,9 +2,11 @@
 import {Head} from "@inertiajs/vue3";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import OfficeManagement from "@/Pages/Appointment/OfficeManagement.vue";
+import AppointmentList from "@/Pages/Appointment/AppointmentList.vue";
 
 const props = defineProps({
-    offices: Array
+    offices: Array,
+    appointments: Array
 });
 </script>
 
@@ -21,10 +23,10 @@ const props = defineProps({
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div class="mt-6">
-                    <OfficeManagement :offices="offices" />
+                    <AppointmentList :appointments="appointments"/>
                 </div>
                 <div class="mt-6">
-                    <!-- add more features here... -->
+                    <OfficeManagement :offices="offices" />
                 </div>
             </div>
         </div>
