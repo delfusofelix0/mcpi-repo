1548cdfabee8cdbba3cabab70f6133afdeb462ca
PerFect <atchupbulad15@gmail.com>
2025-05-09 00:38:40 +0800

    config for display.

---
+++ applicant-app/config/display.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Display Token
+    |--------------------------------------------------------------------------
+    |
+    | This token is used for authenticating display screens.
+    | Keep this value secret and use it only for authorized displays.
+    |
+    */
+    'token' => env('DISPLAY_TOKEN', null),
+];
