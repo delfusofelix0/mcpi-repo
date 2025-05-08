4c1bb425036cfdd609d7a6a4c846c296d51f08a8
PerFect <atchupbulad15@gmail.com>
2025-05-09 00:38:13 +0800

    Redirect to new dashboard.

--- applicant-app/app/Providers/AppServiceProvider.php
+++ applicant-app/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
@@ -25,6 +27,32 @@ class AppServiceProvider extends ServiceProvider
         DB::prohibitDestructiveCommands(
             app()->isProduction()
         );
+
+        RedirectIfAuthenticated::redirectUsing(function ($request) {
+            if (Auth::check()) {
+                $user = Auth::user();
+
+                \Log::info('RedirectIfAuthenticated callback triggered', [
+                    'user_id' => $user->id,
+                    'user_name' => $user->name,
+                    'roles' => $user->getRoleNames()->toArray()
+                ]);
+
+                if ($user->hasRole('Secretary')) {
+                    return route('appointment-settings.index');
+                } elseif ($user->hasRole('Cashier')) {
+                    return route('cashier.dashboard');
+                } elseif ($user->hasRole('Accounting')) {
+                    return route('accounting.dashboard');
+                } elseif ($user->hasRole('Registrar')) {
+                    return route('registrar.dashboard');
+                }
+
+                return route('dashboard');
+            }
+
+            return null;
+        });
     }
 }
 
